71ff99faf108a40572d18c791caf69ecbb9bbd08
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-03-12 18:36:32 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import tailwindLogo from './assets/tailwind.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
+      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+    <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer"> 
+      <img src={tailwindLogo} class="logo tailwind" alt="tailwind Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte + Tailwind</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
